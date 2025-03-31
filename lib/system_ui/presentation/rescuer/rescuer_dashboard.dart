909ab7da493204417a4ef49376b82d686de557a0
joshualfelipe <jlfelipe1@up.edu.ph>
2025-03-31 23:03:29 +0800

    feat: add logout to rescuer dashboard

--- lib/system_ui/presentation/rescuer/rescuer_dashboard.dart
+++ lib/system_ui/presentation/rescuer/rescuer_dashboard.dart
@@ -37,10 +37,26 @@ class RescuerDashboardState extends State<RescuerDashboard> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-          title: Text('Project Buhay'),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black)),
+        title: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text('Project Buhay')),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.logout_outlined,
+                color: Colors.black,
+                textDirection: TextDirection.ltr,
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
